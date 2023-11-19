a530b4c055a612ef9c4544a6a8409ab061d0de98
Renuka Sudha <pavansai1806@gmail.com>
2023-11-19 11:33:16 +0530

    intro/home design done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import About from "./components/About";
+import Home from "./components/Home";
 import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
